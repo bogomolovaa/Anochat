c15bea23899fd1625b75789e0dfc869cd29b1a0d
bogomolovaa <alllexander@gmail.com>
2023-07-02 21:21:19 +0300

    coroutine worker

--- app/src/main/java/bogomolov/aa/anochat/features/contacts/UpdateWorker.kt
+++ app/src/main/java/bogomolov/aa/anochat/features/contacts/UpdateWorker.kt
@@ -2,26 +2,23 @@ package bogomolov.aa.anochat.features.contacts
 
 import android.content.Context
 import androidx.hilt.work.HiltWorker
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import bogomolov.aa.anochat.domain.UserUseCases
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
 
 @HiltWorker
 class UpdateWorker @AssistedInject constructor(
     @Assisted appContext: Context,
     @Assisted workerParams: WorkerParameters,
     private val userUseCases: UserUseCases
-) : Worker(appContext, workerParams) {
+) : CoroutineWorker(appContext, workerParams) {
 
-    override fun doWork(): Result {
-        runBlocking {
-            userUseCases.updateUsersInConversations(true)
-            delay(10 * 1000)
-        }
+    override suspend fun doWork(): Result {
+        userUseCases.updateUsersInConversations(true)
+        delay(10 * 1000)
         return Result.success()
     }
 }
