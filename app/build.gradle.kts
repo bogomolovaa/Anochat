8767b30b7850beb38618ab4b17ddd6d1c9c77368
bogomolovaa <alllexander@gmail.com>
2023-07-10 12:53:09 +0000

    Release v2023.7.56

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "bogomolov.aa.anochat"
         minSdk = 24
         targetSdk = 30
-        versionCode = 89
-        versionName = "2023.7.55"
+        versionCode = 90
+        versionName = "2023.7.56"
         multiDexEnabled = true
         javaCompileOptions {
             annotationProcessorOptions {
